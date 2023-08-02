76dab8be78b085d22664ef918a99e853952d7a59
Olafur Waage <olafurw@gmail.com>
2023-08-03 01:02:11 +0200

    Trying to image match the gems.

--- src/Window.cpp
+++ src/Window.cpp
@@ -3,11 +3,48 @@
 #include "Mouse.hpp"
 #include "Solver.hpp"
 
+void onTrackbarChange(int value, void * object) {
+    if (!object) {
+        return;
+    }
+
+    auto w = static_cast<Window *>(object);
+    w->m_threshold = value;
+}
+
+double distance(const cv::Point & p1, const cv::Point & p2) {
+    const int dx = p1.x - p2.x;
+    const int dy = p1.y - p2.y;
+    return std::sqrt(dx * dx + dy * dy);
+}
+
+std::vector<cv::Point> filterPoints(const std::vector<cv::Point> & points, int distanceThreshold) {
+    std::vector<cv::Point> filteredPoints;
+
+    for (const auto & p1 : points) {
+        bool keepPoint = true;
+
+        for (const auto & p2 : filteredPoints) {
+            if (distance(p1, p2) < distanceThreshold) {
+                keepPoint = false;
+                break;
+            }
+        }
+
+        if (keepPoint) {
+            filteredPoints.push_back(p1);
+        }
+    }
+
+    return filteredPoints;
+}
+
 Window::Window() {
     m_window = FindWindow(NULL, "Bejeweled 3");
     m_stop = false;
     m_width = 0;
     m_height = 0;
+    m_threshold = 50;
 }
 
 void Window::start() {
@@ -15,14 +52,24 @@ void Window::start() {
         return;
     }
 
+    cv::namedWindow("game", cv::WINDOW_NORMAL);
+    cv::createTrackbar("Threshold", "game", &m_threshold, 100, onTrackbarChange, this);
+
+    m_purple = cv::imread("../purple.png", cv::IMREAD_UNCHANGED);
+    m_yellow = cv::imread("../yellow.png", cv::IMREAD_UNCHANGED);
+    m_yellow_fire = cv::imread("../yellow_fire.png", cv::IMREAD_UNCHANGED);
+    m_red = cv::imread("../red.png", cv::IMREAD_UNCHANGED);
+    m_red_fire = cv::imread("../red_fire.png", cv::IMREAD_UNCHANGED);
+    m_red_shiny = cv::imread("../red_shiny.png", cv::IMREAD_UNCHANGED);
+
     if (SetForegroundWindow(m_window) == 0) {
         return;
     }
     
-    for (int i = 0; i < 20; ++i) {
+    while (true) {
         update();
         draw();
-        Sleep(200);
+        Sleep(1);
     }
 }
 
@@ -37,11 +84,73 @@ void Window::update() {
     const Solution & solution = solutionOpt.value();
     const int x = WINDOW_RIGHT_PADDING + (solution.C_point.first * GAME_TILE_SIZE) + GAME_TILE_PADDING;
     const int y = WINDOW_TOP_PADDING + (solution.C_point.second * GAME_TILE_SIZE) + GAME_TILE_PADDING;
-    mouse_click_drag(m_window, x, y, solution.direction.first * GAME_TILE_SIZE, solution.direction.second * GAME_TILE_SIZE);
+    //mouse_click_drag(m_window, x, y, solution.direction.first * GAME_TILE_SIZE, solution.direction.second * GAME_TILE_SIZE);
 }
 
 void Window::draw() {
-    //imshow("GAME", m_mat);
+    std::vector<cv::Point> matchLocations;
+    {
+        cv::Mat correlationMap;
+        int matchMethod = CV_TM_CCOEFF_NORMED; // You can choose a different matching method if needed
+        cv::matchTemplate(m_mat, m_red, correlationMap, matchMethod);
+
+        double threshold = static_cast<double>(m_threshold / 100.0);
+        cv::Mat thresholdMap;
+        cv::threshold(correlationMap, thresholdMap, threshold, 1.0, cv::THRESH_TOZERO);
+
+        std::vector<cv::Point> tmpLocations;
+        cv::findNonZero(thresholdMap, tmpLocations);
+
+        matchLocations = filterPoints(tmpLocations, 30);
+
+        for (const auto & loc : matchLocations) {
+            cv::Rect boundingBox(loc.x, loc.y, m_red.cols, m_red.rows);
+            cv::rectangle(m_mat, boundingBox, cv::Scalar(0, 255, 0), 2);
+        }
+    }
+    {
+        cv::Mat correlationMap;
+        int matchMethod = CV_TM_CCOEFF_NORMED; // You can choose a different matching method if needed
+        cv::matchTemplate(m_mat, m_red_fire, correlationMap, matchMethod);
+
+        double threshold = static_cast<double>(m_threshold / 100.0);
+        cv::Mat thresholdMap;
+        cv::threshold(correlationMap, thresholdMap, threshold, 1.0, cv::THRESH_TOZERO);
+
+        std::vector<cv::Point> tmpLocations;
+        cv::findNonZero(thresholdMap, tmpLocations);
+
+        matchLocations.insert(matchLocations.end(), tmpLocations.begin(), tmpLocations.end());
+        matchLocations = filterPoints(matchLocations, 30);
+
+        for (const auto & loc : matchLocations) {
+            cv::Rect boundingBox(loc.x, loc.y, m_red_fire.cols, m_red_fire.rows);
+            cv::rectangle(m_mat, boundingBox, cv::Scalar(0, 255, 0), 2);
+        }
+    }
+    {
+        cv::Mat correlationMap;
+        int matchMethod = CV_TM_CCOEFF_NORMED; // You can choose a different matching method if needed
+        cv::matchTemplate(m_mat, m_red_shiny, correlationMap, matchMethod);
+
+        double threshold = static_cast<double>(m_threshold / 100.0);
+        cv::Mat thresholdMap;
+        cv::threshold(correlationMap, thresholdMap, threshold, 1.0, cv::THRESH_TOZERO);
+
+        std::vector<cv::Point> tmpLocations;
+        cv::findNonZero(thresholdMap, tmpLocations);
+
+        matchLocations.insert(matchLocations.end(), tmpLocations.begin(), tmpLocations.end());
+        matchLocations = filterPoints(matchLocations, 30);
+
+        for (const auto & loc : matchLocations) {
+            cv::Rect boundingBox(loc.x, loc.y, m_red_shiny.cols, m_red_shiny.rows);
+            cv::rectangle(m_mat, boundingBox, cv::Scalar(0, 255, 0), 2);
+        }
+    }
+
+    cv::imshow("game", m_mat);
+    cv::waitKey(1);
 }
 
 // http://stackoverflow.com/questions/14148758/how-to-capture-the-desktop-in-opencv-ie-turn-a-bitmap-into-a-mat
@@ -59,6 +168,7 @@ void Window::window_to_mat() {
 
     m_mat.release();
     m_mat.create(height, width, CV_8UC4);
+    const auto x = m_mat.type();
 
     HBITMAP hbwindow = CreateCompatibleBitmap(hwindowDC, width, height);
 
--- src/Window.hpp
+++ src/Window.hpp
@@ -5,7 +5,9 @@
 
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/imgcodecs/imgcodecs.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/types_c.h"
 
 #include "Pattern.hpp"
 
@@ -22,10 +24,17 @@ class Window {
   public:
     Window();
     void start();
+    int m_threshold;
 
   private:
     HWND m_window;
     cv::Mat m_mat;
+    cv::Mat m_purple;
+    cv::Mat m_yellow;
+    cv::Mat m_yellow_fire;
+    cv::Mat m_red;
+    cv::Mat m_red_fire;
+    cv::Mat m_red_shiny;
     Patterns m_patterns;
 
     int m_width;
